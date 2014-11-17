e4a6a8c45fddaec0dff4b0c8c7914463b63aaea0
Steve Springett <steve@Prometheus.local>
2014-11-17 09:10:02 -0600

    Updated URLs to support new gz downloads available from NIST. Added auto uncompress after successful download.

--- src/NistDataMirror.java
+++ src/NistDataMirror.java
@@ -14,18 +14,20 @@
  * You should have received a copy of the GNU General Public License along with
  * nist-data-mirror. If not, see http://www.gnu.org/licenses/.
  *
- * Copyright (c) 2013 Steve Springett. All Rights Reserved.
+ * Copyright (c) 2014 Steve Springett. All Rights Reserved.
  */
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.Closeable;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.zip.GZIPInputStream;
 
 /**
  * This self-contained class can be called from the command-line. It downloads the
@@ -35,10 +37,10 @@ import java.util.Date;
  */
 public class NistDataMirror {
 
-    private static final String CVE_12_MODIFIED_URL = "http://nvd.nist.gov/download/nvdcve-modified.xml";
-    private static final String CVE_20_MODIFIED_URL = "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-modified.xml";
-    private static final String CVE_12_BASE_URL = "http://nvd.nist.gov/download/nvdcve-%d.xml";
-    private static final String CVE_20_BASE_URL = "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-%d.xml";
+    private static final String CVE_12_MODIFIED_URL = "https://nvd.nist.gov/download/nvdcve-Modified.xml.gz";
+    private static final String CVE_20_MODIFIED_URL = "https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-Modified.xml.gz";
+    private static final String CVE_12_BASE_URL = "https://nvd.nist.gov/download/nvdcve-%d.xml.gz";
+    private static final String CVE_20_BASE_URL = "https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-%d.xml.gz";
     private static final int START_YEAR = 2002;
     private static final int END_YEAR = Calendar.getInstance().get(Calendar.YEAR);
     private File outputDir;
@@ -82,7 +84,8 @@ public class NistDataMirror {
     private void doDownload(String cveUrl) {
         BufferedInputStream bis = null;
         BufferedOutputStream bos = null;
-
+        File file = null;
+        boolean success = false;
         try {
             URL url = new URL(cveUrl);
             URLConnection urlConnection = url.openConnection();
@@ -92,19 +95,42 @@ public class NistDataMirror {
             filename = filename.substring(filename.lastIndexOf('/') + 1);
 
             bis = new BufferedInputStream(urlConnection.getInputStream());
-            File file = new File(outputDir, filename);
+            file = new File(outputDir, filename);
             bos = new BufferedOutputStream(new FileOutputStream(file));
 
             int i;
             while ((i = bis.read()) != -1) {
-                bos.write( i );
+                bos.write(i);
             }
+            success = true;
         } catch (IOException e) {
             System.out.println("Download failed : " + e.getLocalizedMessage());
             downloadFailed = true;
         } finally {
             close(bis);
             close(bos);
+        }
+        if (file != null && success)
+            uncompress(file);
+    }
+
+    public void uncompress(File file) {
+        byte[] buffer = new byte[1024];
+        GZIPInputStream gzis = null;
+        FileOutputStream out = null;
+        try{
+            System.out.println("Uncompressing " + file.getName());
+            gzis = new GZIPInputStream(new FileInputStream(file));
+            out = new FileOutputStream(new File(file.getAbsolutePath().replaceAll(".gz", "")));
+            int len;
+            while ((len = gzis.read(buffer)) > 0) {
+                out.write(buffer, 0, len);
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        } finally {
+            close(gzis);
+            close(out);
         }
     }
 
@@ -118,4 +144,3 @@ public class NistDataMirror {
         }
     }
 }
-
