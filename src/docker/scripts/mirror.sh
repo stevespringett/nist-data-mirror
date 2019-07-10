0a56fc08a91b4b3b2810f9966c110134c3be5e20
Emanuel Buholzer <contact@emanuelbuholzer.com>
2019-07-10 17:27:36 +0200

    Respect Docker memory limits

--- src/docker/scripts/mirror.sh
+++ src/docker/scripts/mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=`java -jar -Dhttp.proxyHost="${proxy_host}" -Dhttp.proxyPort="${proxy_port}" /usr/local/bin/nist-data-mirror.jar /tmp/nvd | grep -Eo '(Download succeeded|Uncompressed).*' | grep -Eo '[^ ]*\.(gz|meta|json|xml)'`
+files=`java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar -Dhttp.proxyHost="${proxy_host}" -Dhttp.proxyPort="${proxy_port}" /usr/local/bin/nist-data-mirror.jar /tmp/nvd | grep -Eo '(Download succeeded|Uncompressed).*' | grep -Eo '[^ ]*\.(gz|meta|json|xml)'`
 
 timestamp=$(date +%s)
 
